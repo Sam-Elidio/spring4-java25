e7ac6bcd667debd3577dbb4a99b73dc475c31e84
Samuel Elidio <elidiosamuel@icloud.com>
2026-03-12 15:53:16 -0300

    Adicionado endpoints getAllUsers e findById

--- src/main/java/com/elidios/java/resourses/UserResource.java
+++ src/main/java/com/elidios/java/resourses/UserResource.java
@@ -1,18 +1,32 @@
 package com.elidios.java.resourses;
 
 import com.elidios.java.entities.User;
+import com.elidios.java.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    UserService userService;
+
     @GetMapping
-    public ResponseEntity<User> finAll(){
-        User u = new User(Long.valueOf(1), "Maria", "maria@gmail.com", "99999999999", "12345");
-    return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> finAll(){
+        List<User> list = userService.getAllUsers();
+    return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(user);
     }
 }
---
+++ src/main/java/com/elidios/java/services/UserService.java
@@ -0,0 +1,25 @@
+package com.elidios.java.services;
+
+import com.elidios.java.entities.User;
+import com.elidios.java.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = userRepository.findById(id);
+        return obj.get();
+    }
+}
